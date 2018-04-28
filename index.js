b823d59fd21e4d9996e9642b41bd326def75e0cf
Pablo Mendez <pablomm@lagash.com>
2018-04-28 19:39:48 -0300

    added passport, register, login and logout

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express();
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 
 const keys = require('./config/keys')
+const app = express();
+
 require('./models/User')
+require('./services/passport')
 
 mongoose.Promise = global.Promise
 mongoose.connect(keys.mongoURI)
+
+app.use(bodyParser.json())
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 1000,
+        keys: [keys.cookieSecret]
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
+require('./routes/error')(app)
 
 app.use(express.static(__dirname + '/client'))
 
