36b0b2109e043127f265e06f4d6c2eed1466d8f5
Pablo Mendez <pablomm@lagash.com>
2018-04-28 17:35:29 -0300

    added mongo

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
+
+const keys = require('./config/keys')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.mongoURI)
 
 app.use(express.static(__dirname + '/client'))
 
